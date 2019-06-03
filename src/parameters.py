9ead1140788ade8538807d4afcf6b33dbb46e7df
Nikita Demir <nikitde1@gmail.com>
2019-06-03 16:45:34 -0700

    Mixed around how we do model stuff

---
+++ src/parameters.py
@@ -0,0 +1,80 @@
+DATASET = 'Call'
+#DATASET = 'Activate'
+#DATASET = 'MFCC_Call'
+
+NORM = "Norm"
+SCALE = True
+
+HYPERPARAMETERS = {
+0: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+1: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+2: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+3: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+4: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+5: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+6: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+7: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+8: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+9: {
+        'lr': 1e-3,
+        'lr_decay_step': 4,
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        }
+}
+
+RANDOM_SEED = 42
+
+BATCH_SIZE = 32
+NUM_EPOCHS = 1000
+
+MODEL_SAVE_PATH = '../weights/'
+LOGS_SAVE_PATH = './runs/'
+
+INPUT_SIZE = 77
+OUTPUT_SIZE = 1
