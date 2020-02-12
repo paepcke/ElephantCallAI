02c7806fef4d8e6185962445ddf320e46ec9246e
Nikita Demir <nikitde1@gmail.com>
2020-02-11 21:25:47 -0800

    A

--- src/process_rawdata_new.py
+++ src/process_rawdata_new.py
@@ -401,8 +401,8 @@ if __name__ == '__main__':
 
             # Catch case where no calls exist so the gt file does not
             label_path = curren_dir + '/' + label_file if label_file is not None else None
-            full_24_hr_spectogram = generate_spectograms.generate_whole_spectogram(curren_dir + '/' + audio_file, spectrogram_info, -1)
-            labels = generate_spectograms.generate_labels(label_path, spectrogram_info, full_24_hr_spectogram.shape[0])
+            full_24_hr_spectogram = generate_spectrograms.generate_whole_spectogram(curren_dir + '/' + audio_file, spectrogram_info, -1)
+            labels = generate_spectrograms.generate_labels(label_path, spectrogram_info, full_24_hr_spectogram.shape[0])
 
             feature_set = np.array_split(full_24_hr_spectogram, 256)[:-1]
             label_set = np.array_split(labels, 256)[:-1]
