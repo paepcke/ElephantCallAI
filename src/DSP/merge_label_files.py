cd64d1b8c1f3378cd8ab1e6285b0f443fd0dc555
Andreas Paepcke <paepcke@cs.stanford.edu>
2020-02-25 16:45:52 -0800

    In merge_label_files.py, corrected file localization, and time/freq range addition

--- src/DSP/merge_label_files.py
+++ src/DSP/merge_label_files.py
@@ -8,7 +8,9 @@ import argparse
 import csv
 import os
 from pathlib import Path
+import pathlib
 import sys
+
 
 class LabelMerger(object):
     '''
@@ -26,6 +28,7 @@ class LabelMerger(object):
         
         all_label_files = []
         for label_dir in dir_paths:
+            # Find all label files
             if not os.path.exists(label_dir):
                 print(f"Label dir {label_dir} does not exist; skipping.")
                 continue
@@ -35,9 +38,13 @@ class LabelMerger(object):
                            if Path(label_file).suffix in ['.txt', '.tsv', '.csv']
                            ]
             full_label_paths = [os.path.join(label_dir,
-                                              label_file) for label_file in label_files]
+                                             label_file) for label_file in label_files]
             all_label_files.extend(full_label_paths)
+        
+        # Get array of label row, with .wav file localized,
+        # and time/freq ranges added: 
         all_label_rows = self.merge_paths(full_label_paths)
+
         if outfile is None:
             self.out_result(all_label_rows, sys.stdout)
         else:
@@ -59,20 +66,94 @@ class LabelMerger(object):
     #-------------------    
 
     def merge_paths(self, full_label_paths):
+        '''
+        Takes list of full paths to label files. 
+        Returns an array of rows from all the labels
+        combined. The 'Begin Path' to the corresponding
+            .wav file is updated to be the local path.
+            Columns 'Time Range' and 'Freq Range' are added
+            to each row.
+        
+        @param full_label_paths: paths to label files to combine
+        @type full_label_paths: (str)
+        '''
         
         all_label_rows = []
         
         for label_path in full_label_paths:
+            curr_path_dir = os.path.dirname(label_path)
             with open(label_path,'r') as csv_fd:
                 reader = csv.reader(csv_fd, delimiter='\t')
                 # Read the col headers, and keep them only 
                 # from the first label file:
                 header = next(reader)
                 if len(all_label_rows) == 0:
+                    # Anticipate adding time and freq ranges:
+                    header.extend(['Time Range', 'Freq Range'])
                     all_label_rows.append(header)
-                all_label_rows.extend([row for row in reader])
+                    try:
+                        self.begin_time_pos    = header.index('Begin Time (s)')
+                        self.end_time_pos      = header.index('End Time (s)')
+                        self.low_freq_pos      = header.index('Low Freq (Hz)')
+                        self.high_freq_pos     = header.index('High Freq (Hz)')
+                        self.full_wav_path_pos = header.index('Begin Path')
+                    except ValueError as e:
+                        # For this label file the headers aren't what
+                        # we expect:
+                        print(f"Header file of {label_path} missing a column: {repr(e)}; ignoring file.")
+                        continue
+                    
+                for row in reader:
+                    # Localize the file path, and add frequency
+                    # and time spans: 
+                    self.enrich_row(row, curr_path_dir)
+                    all_label_rows.append(row)
+
         return all_label_rows
-    
+
+    #------------------------------------
+    # enrich_row
+    #-------------------    
+
+    def enrich_row(self, label_row, label_dir):
+        '''
+        Given a label row:
+            1. Change the Cornell file path to local file path
+            2. Add frequency range column ('Freq Range')
+            3. Add time range column ('Time Range')
+            
+        Assumptions: the following instance vars are 
+        indexes into the row:
+        
+            self.begin_time_pos
+            self.end_time_pos
+            self.low_freq_pos
+            self.high_freq_pos
+            self.full_wav_path_pos
+
+        @param label_row: label data
+        @type label_row: [str]
+        @param label_dir: directory of the label file from where row 
+            originated
+        @type label_dir: str
+        @return: new label row
+        @rtype: [str]
+        '''
+        cornell_file_path = label_row[self.full_wav_path_pos]
+        if cornell_file_path.find('\\') > -1:
+            # Windows path:
+            cornell_file_path = pathlib.PureWindowsPath(cornell_file_path)
+        else:
+            cornell_file_path = pathlib.PurePath(cornell_file_path)
+            
+        file_name  = cornell_file_path.name
+        local_path = os.path.join(label_dir, file_name)
+        label_row[self.full_wav_path_pos] = local_path
+        time_range = str(float(label_row[self.end_time_pos]) - float(label_row[self.begin_time_pos]))
+        freq_range = str(float(label_row[self.high_freq_pos]) - float(label_row[self.low_freq_pos]))
+        label_row.extend([time_range, freq_range])
+        return label_row
+        
 # ----------------------------- Main ----------------
 
 if __name__ == '__main__':
