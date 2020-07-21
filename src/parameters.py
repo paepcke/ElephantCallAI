c76dad12c859f1dba7fe953d6d3256d2f56c3dd5
jonathangomesselman <jgs8@stanford.edu>
2020-07-21 18:14:28 -0400

    Adding new CRNN

--- src/parameters.py
+++ src/parameters.py
@@ -36,7 +36,7 @@ HIERARCHICAL_REPEATS = 1
 HIERARCHICAL_SHIFT_WINDOWS = False
 
 # Model 18 = entire window classification
-MODEL_ID = 20
+MODEL_ID = 21
 
 # WE SHOULD PHASE THIS OUT!
 DATASET = 'Call'
@@ -216,6 +216,12 @@ HYPERPARAMETERS = {
         'l2_reg': 1e-5,
         },
 20: {
+        'lr': 1e-3,
+        'lr_decay_step': 4, 
+        'lr_decay': 0.95,
+        'l2_reg': 1e-5,
+        },
+21: {
         'lr': 1e-3,
         'lr_decay_step': 4, 
         'lr_decay': 0.95,
