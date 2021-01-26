7cc67fed4a3dab17a648b6248ee27161b2f77c77
Danny Schwartz <deschwa2@illinois.edu>
2021-01-26 14:30:24 -0800

    Implement simple timestamp queue mechanism for associating buffer regions with times of audio recording
    Eventually, we will need separate timestamp queues for unprocessed data and data awaiting post-processing.

--- src/embedded/SpectrogramBuffer.py
+++ src/embedded/SpectrogramBuffer.py
@@ -1,6 +1,8 @@
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Deque
 
 import numpy as np
+from collections import deque
+from datetime import datetime, timezone, timedelta
 
 
 # defaults
@@ -11,15 +13,25 @@ BUFFER_SIZE_MB = 512  # This is configurable, it's how much dedicated storage sp
 BYTES_PER_ELEMENT = 8  # We're using float64
 BUFFER_SIZE_IN_TIME_STEPS = BUFFER_SIZE_MB * 1024 * 1024 // (FREQ_BINS * BYTES_PER_ELEMENT)
 
+# It won't make sense to run the model on spectrogram data with fewer than this number of time steps.
+# This means that appending fewer than this number of contiguous time steps is disallowed.
+# This constant is the default value, but other values can be configured in the constructor.
+MIN_APPENDABLE_TIME_STEPS = NUM_SAMPLES_IN_TIME_WINDOW
+
+# TODO: figure this out by looking at other places in the codebase, current value is a placeholder
+TIME_DELTA_PER_TIME_STEP = timedelta(seconds=1)
+
 """A thread-safe spectrogram buffer."""
 class SpectrogramBuffer:
     buffer: np.ndarray  # The first dimension is time steps, the second is frequency bins
 
     # an index into the first dimension of the buffer indicating the data between 'processed' and this has not been evaluated by the model
+    # the data at this time row may only be unprocessed if the entire buffer is full.
     unprocessed_end: int
 
     # an index into the first dimension of the buffer indicating the data between 'end' and this has been evaluated by the model
     # but should not be discarded yet
+    # the data at this time row is only pending post processing if the entire buffer is full and all of it is pending post processing.
     pending_post_processing_end: int
 
     # data before this point is garbage and may be freely overwritten.
@@ -31,9 +43,11 @@ class SpectrogramBuffer:
     # The number of rows currently allocated to storing data
     rows_allocated: int
 
-    # TODO: create a timestamp <-> array index scheme, perhaps with an AVL tree
+    # A queue of tuples that allow us to keep track of the timestamps corresponding to examples in the queue.
+    # TODO: a deque for each state (unprocessed, post-processing)
+    timestamp_deque: Deque[Tuple[int, datetime]]
 
-    def __init__(self, override_buffer_size: Optional[int] = None):
+    def __init__(self, override_buffer_size: Optional[int] = None, min_appendable_time_steps: Optional[int] = None):
         buf_size = BUFFER_SIZE_IN_TIME_STEPS if override_buffer_size is None else override_buffer_size
         self.buffer = np.zeros((buf_size, FREQ_BINS))
         self.unprocessed_end = 0
@@ -41,11 +55,15 @@ class SpectrogramBuffer:
         self.allocated_begin = 0
         self.rows_allocated = 0
         self.rows_unprocessed = 0
+        self.timestamp_deque = deque()
+        if min_appendable_time_steps is None:
+            self.min_appendable_time_steps = MIN_APPENDABLE_TIME_STEPS
+        else:
+            self.min_appendable_time_steps = min_appendable_time_steps
 
 
     # TODO: an approach where the STFT writes directly into this buffer?
-    # TODO: take timestamp as a parameter?
-    def append_data(self, new_data: np.ndarray):
+    def append_data(self, new_data: np.ndarray, time: Optional[datetime] = None):
         """Append new unprocessed data to the buffer."""
         new_data_time_len = new_data.shape[0]
         # TODO: synchronization between different threads
@@ -54,6 +72,10 @@ class SpectrogramBuffer:
         if available_time_distance_in_buf < new_data_time_len:
             # TODO: handle this more gracefully
             raise ValueError("Not enough space in the buffer for new data!")
+        self._assert_sufficient_time_steps(new_data, time)
+
+        # remember this for timestamp updates
+        data_start_idx = self.unprocessed_end
 
         if self.unprocessed_end + new_data_time_len > self.buffer.shape[0]:
             first_len = self.buffer.shape[0] - self.unprocessed_end
@@ -67,6 +89,20 @@ class SpectrogramBuffer:
         self.rows_allocated += new_data_time_len
         self.rows_unprocessed += new_data_time_len
 
+        # handle the timestamp
+        if time is None and len(self.timestamp_deque) == 0:
+            now = datetime.now(timezone.utc)
+            self.timestamp_deque.append((data_start_idx, now))
+        elif time is not None:
+            self.timestamp_deque.append((data_start_idx, time))
+        # If time is None, this class assumes that the recording of this data occurred immediately after
+        # the recording of the previous data, without interruption.
+
+    def _assert_sufficient_time_steps(self, new_data: np.ndarray, time: Optional[datetime]):
+        if new_data.shape[0] < self.min_appendable_time_steps:
+            raise ValueError(("Cannot put less than {} contiguous time steps of data " +
+                             "into the buffer in a single append operation.").format(self.min_appendable_time_steps))
+
     # data should always be accessed from the *return value* of this method
     def get_unprocessed_data(self, time_steps: int, target: Optional[np.ndarray] = None, mark_for_postprocessing: bool = True) -> np.ndarray:
         """Read unprocessed data from the buffer. By default, this read marks the data as 'processed'."""
@@ -79,10 +115,13 @@ class SpectrogramBuffer:
     def get_processed_data(self, time_steps: int, target: Optional[np.ndarray] = None, free_buffer_region: bool = True) -> np.ndarray:
         """Read data from the buffer for post-processing. This copies the data into a separate location
         and, by default, frees the region of the buffer for other use."""
+        # TODO: block this operation until there is a meaningful number of rows left for processing
         data, new_begin_idx = self.consume_data(time_steps, True, target=target, force_copy=True)
         if free_buffer_region:
             self.rows_allocated -= time_steps
+            old_allocated_begin = self.allocated_begin
             self.allocated_begin = new_begin_idx
+            self._free_rows_update_timestamp(old_allocated_begin)
         return data
 
     def mark_post_processing_complete(self, time_steps: int) -> int:
@@ -92,8 +131,37 @@ class SpectrogramBuffer:
         if time_steps > available_time_steps:
             time_steps = available_time_steps
         self.rows_allocated -= time_steps
+        old_allocated_begin = self.allocated_begin
         self.allocated_begin = (self.allocated_begin + time_steps) % self.buffer.shape[0]
+        self._free_rows_update_timestamp(old_allocated_begin)
         return time_steps
+
+    # update 'rows_allocated' and 'allocated_begin' BEFORE calling this method
+    def _free_rows_update_timestamp(self, old_allocated_begin: int):
+        if self.rows_allocated == 0:
+            self.timestamp_deque.clear()
+            return
+
+        new_allocated_begin = self.allocated_begin
+        # take elements off the timestamp queue until we go past or hit the new allocated beginning, then, assuming
+        # the timestamp isn't exact, add the time delta for how many rows after the most recently deleted timestamp were
+        # freed, then re-queue that timestamp.
+        max_distance = (new_allocated_begin - old_allocated_begin) % self.buffer.shape[0]
+        cur_timestamp_entry = self.timestamp_deque.popleft()
+        next_timestamp_distance = 0
+        if len(self.timestamp_deque) != 0:
+            next_timestamp_distance = self._circular_distance(self.timestamp_deque[0][0], old_allocated_begin)
+        while len(self.timestamp_deque) != 0 and next_timestamp_distance <= max_distance:
+            cur_timestamp_entry = self.timestamp_deque.popleft()
+            next_timestamp_distance = self._circular_distance(self.timestamp_deque[0][0], old_allocated_begin)
+
+        new_timestamp = (new_allocated_begin,
+                         cur_timestamp_entry[1]
+                         + TIME_DELTA_PER_TIME_STEP * (self._circular_distance(new_allocated_begin, cur_timestamp_entry[0])))
+        self.timestamp_deque.appendleft(new_timestamp)
+
+    def _circular_distance(self, right_idx: int, left_idx: int) -> int:
+        return (right_idx - left_idx) % self.buffer.shape[0]
 
     # copy data from the buffer into the target if necessary, otherwise, return a view.
     # returns (read data, new value for starting index of read-from chunk)
@@ -138,3 +206,4 @@ class SpectrogramBuffer:
         self.allocated_begin = 0
         self.rows_allocated = 0
         self.rows_unprocessed = 0
+        self.timestamp_deque.clear()
--- test/embedded/SpectrogramBufferTest.py
+++ test/embedded/SpectrogramBufferTest.py
@@ -1,14 +1,15 @@
 import unittest
 import numpy as np
+from datetime import datetime, timezone, timedelta
 
-from src.embedded.SpectrogramBuffer import SpectrogramBuffer
+from src.embedded.SpectrogramBuffer import SpectrogramBuffer, TIME_DELTA_PER_TIME_STEP
 
 
 class SpectrogramBufferTest(unittest.TestCase):
     """Unit tests for the SpectrogramBuffer class."""
 
     def test_basic_write_into_buffer(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         new_data = np.arange(3, dtype=float).reshape(-1, 1)
         buffer.append_data(new_data)
         self.assertEqual(3, buffer.unprocessed_end)
@@ -17,7 +18,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.assertTrue(np.array_equal(buffer.buffer[0:3, 12].reshape(-1, 1), new_data))
 
     def test_basic_read_from_buffer(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         new_data = np.arange(3, dtype=float).reshape(-1, 1)
         buffer.append_data(new_data)
         self.assertEqual(3, buffer.unprocessed_end)
@@ -30,7 +31,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.assertEqual(0, buffer.allocated_begin)
 
     def test_basic_read_from_buffer_without_marking_data_as_processed(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         new_data = np.arange(3, dtype=float).reshape(-1, 1)
         buffer.append_data(new_data)
         self.assertEqual(3, buffer.unprocessed_end)
@@ -43,7 +44,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.assertEqual(0, buffer.allocated_begin)
 
     def test_wraparound_writing(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         buffer.unprocessed_end = 6
         buffer.pending_post_processing_end = 6
         buffer.allocated_begin = 6
@@ -56,7 +57,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.assertEqual(2, buffer.buffer[0, 12])
 
     def test_wraparound_read(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         buffer.unprocessed_end = 6
         buffer.pending_post_processing_end = 6
         buffer.allocated_begin = 6
@@ -74,33 +75,38 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.assertEqual(6, buffer.allocated_begin)
 
     def test_free_memory(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         buffer.unprocessed_end = 5
         buffer.pending_post_processing_end = 3
         buffer.rows_unprocessed = 2
         buffer.rows_allocated = 5
+        buffer.timestamp_deque.append((0, datetime.now(timezone.utc)))
 
         buffer.mark_post_processing_complete(2)
+        self.assertEqual(2, buffer.allocated_begin)
         self.assertEqual(5, buffer.unprocessed_end)
         self.assertEqual(3, buffer.pending_post_processing_end)
         self.assertEqual(3, buffer.rows_allocated)
         self.assertEqual(2, buffer.rows_unprocessed)
 
     def test_free_memory_wraparound(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         buffer.unprocessed_end = 4
         buffer.pending_post_processing_end = 2
+        buffer.allocated_begin = 4
         buffer.rows_unprocessed = 2
         buffer.rows_allocated = 8
+        buffer.timestamp_deque.append((4, datetime.now(timezone.utc)))
 
         buffer.mark_post_processing_complete(4)
+        self.assertEqual(0, buffer.allocated_begin)
         self.assertEqual(4, buffer.unprocessed_end)
         self.assertEqual(2, buffer.pending_post_processing_end)
         self.assertEqual(4, buffer.rows_allocated)
         self.assertEqual(2, buffer.rows_unprocessed)
 
     def test_exception_on_write_too_big(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         new_data = np.arange(10, dtype=float).reshape(-1, 1)
 
         try:
@@ -111,7 +117,7 @@ class SpectrogramBufferTest(unittest.TestCase):
 
     # Note: In the case of a wraparound, the output data will be a copy. This test does not cover that case.
     def test_read_unprocessed_data_doesnt_copy(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         buffer.unprocessed_end = 2
         buffer.rows_unprocessed = 2
         buffer.rows_allocated = 2
@@ -122,7 +128,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.assertEqual(4, buffer.buffer[0, 0])
 
     def test_read_for_postprocessing_makes_copy(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         buffer.buffer[0, 0] = 3
         buffer.unprocessed_end = 2
         buffer.pending_post_processing_end = 2
@@ -134,7 +140,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.assertNotEqual(data_out[0, 0], buffer.buffer[0, 0])
 
     def test_completely_full_buffer_disallows_writes(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         new_data = np.arange(8, dtype=float).reshape(-1, 1)
         buffer.append_data(new_data)
 
@@ -145,7 +151,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.fail("Expected exception but none occurred")
 
     def test_cannot_free_unprocessed_data(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         new_data = np.arange(8, dtype=float).reshape(-1, 1)
         buffer.append_data(new_data)
 
@@ -153,7 +159,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.assertEqual(0, rows_freed)
 
     def test_free_memory_allows_overwrite(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         new_data = np.arange(8, dtype=float).reshape(-1, 1)
         buffer.append_data(new_data)
         buffer.get_unprocessed_data(3)
@@ -162,7 +168,7 @@ class SpectrogramBufferTest(unittest.TestCase):
         buffer.append_data(np.zeros((2, 1)))
 
     def test_exception_thrown_when_reading_unavailable_processed_data(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         new_data = np.arange(8, dtype=float).reshape(-1, 1)
         buffer.append_data(new_data)
         try:
@@ -172,12 +178,129 @@ class SpectrogramBufferTest(unittest.TestCase):
         self.fail("Expected exception but none thrown.")
 
     def test_exception_thrown_when_reading_unavailable_unprocessed_data(self):
-        buffer = SpectrogramBuffer(override_buffer_size=8)
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
         try:
             buffer.get_unprocessed_data(3)
         except ValueError:
             return
         self.fail("Expected exception but none thrown.")
+
+    def test_ex_thrown_when_queueing_insufficient_data(self):
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=3)
+        new_data = np.arange(2, dtype=float).reshape(-1, 1)
+
+        try:
+            buffer.append_data(new_data)
+        except ValueError:
+            return
+        self.fail("Expected exception but none thrown")
+
+    def test_free_all_memory_empties_timestamp_deque(self):
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
+        buffer.unprocessed_end = 3
+        buffer.pending_post_processing_end = 3
+        buffer.rows_unprocessed = 0
+        buffer.rows_allocated = 3
+        buffer.timestamp_deque.append((0, datetime.now(timezone.utc)))
+
+        buffer.mark_post_processing_complete(3)
+        self.assertEqual(3, buffer.allocated_begin)
+        self.assertEqual(3, buffer.unprocessed_end)
+        self.assertEqual(3, buffer.pending_post_processing_end)
+        self.assertEqual(0, buffer.rows_allocated)
+        self.assertEqual(0, buffer.rows_unprocessed)
+
+        self.assertEqual(0, len(buffer.timestamp_deque))
+
+    def test_timestamp_queue_updates_freeing_data(self):
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
+        buffer.unprocessed_end = 5
+        buffer.pending_post_processing_end = 3
+        buffer.rows_unprocessed = 2
+        buffer.rows_allocated = 5
+        now = datetime.now(timezone.utc)
+        buffer.timestamp_deque.append((0, now))
+        buffer.mark_post_processing_complete(3)
+
+        self.assertEqual(1, len(buffer.timestamp_deque))
+
+        top_timestamp = buffer.timestamp_deque[0]
+        self.assertEqual(3, top_timestamp[0])
+        expected_time = now + 3 * TIME_DELTA_PER_TIME_STEP
+        self.assertEqual(expected_time, top_timestamp[1])
+
+    def test_timestamp_queue_updates_freeing_data_pruning_multiple_timestamps(self):
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
+        buffer.unprocessed_end = 7
+        buffer.pending_post_processing_end = 6
+        buffer.rows_unprocessed = 1
+        buffer.rows_allocated = 7
+        now = datetime.now(timezone.utc)
+        time2 = now + 10*timedelta(seconds=2)
+        time3 = time2 + 18*timedelta(seconds=3)
+        time4 = time3 + 7*timedelta(seconds=1)
+        buffer.timestamp_deque.append((0, now))
+        buffer.timestamp_deque.append((2, time2))
+        buffer.timestamp_deque.append((4, time3))
+        buffer.timestamp_deque.append((6, time4))
+
+        buffer.mark_post_processing_complete(5)
+
+        self.assertEqual(2, len(buffer.timestamp_deque))
+        top_timestamp = buffer.timestamp_deque[0]
+        self.assertEqual(5, top_timestamp[0])
+        expected_time = time3 + 1 * TIME_DELTA_PER_TIME_STEP
+        self.assertEqual(expected_time, top_timestamp[1])
+
+        self.assertEqual((6, time4), buffer.timestamp_deque[1])
+
+    def test_timestamp_queue_updates_freeing_data_pruning_multiple_timestamps_with_wraparound(self):
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
+        buffer.unprocessed_end = 3
+        buffer.pending_post_processing_end = 2
+        buffer.allocated_begin = 4
+        buffer.rows_unprocessed = 1
+        buffer.rows_allocated = 7
+        now = datetime.now(timezone.utc)
+        time2 = now + 10 * timedelta(seconds=2)
+        time3 = time2 + 18 * timedelta(seconds=3)
+        buffer.timestamp_deque.append((5, now))
+        buffer.timestamp_deque.append((0, time2))
+        buffer.timestamp_deque.append((2, time3))
+
+        buffer.mark_post_processing_complete(5)
+
+        self.assertEqual(2, len(buffer.timestamp_deque))
+        top_timestamp = buffer.timestamp_deque[0]
+        self.assertEqual(1, top_timestamp[0])
+        expected_time = time2 + 1 * TIME_DELTA_PER_TIME_STEP
+        self.assertEqual(expected_time, top_timestamp[1])
+
+        self.assertEqual((2, time3), buffer.timestamp_deque[1])
+
+    def test_timestamp_queue_updates_freeing_data_exact_new_timestamp(self):
+        buffer = SpectrogramBuffer(override_buffer_size=8, min_appendable_time_steps=1)
+        buffer.unprocessed_end = 3
+        buffer.pending_post_processing_end = 2
+        buffer.allocated_begin = 4
+        buffer.rows_unprocessed = 1
+        buffer.rows_allocated = 7
+        now = datetime.now(timezone.utc)
+        time2 = now + 10 * timedelta(seconds=2)
+        time3 = time2 + 18 * timedelta(seconds=3)
+        buffer.timestamp_deque.append((5, now))
+        buffer.timestamp_deque.append((0, time2))
+        buffer.timestamp_deque.append((2, time3))
+
+        buffer.mark_post_processing_complete(4)
+
+        self.assertEqual(2, len(buffer.timestamp_deque))
+        top_timestamp = buffer.timestamp_deque[0]
+        self.assertEqual(0, top_timestamp[0])
+        expected_time = time2
+        self.assertEqual(expected_time, top_timestamp[1])
+
+        self.assertEqual((2, time3), buffer.timestamp_deque[1])
 
 
 if __name__ == '__main__':
