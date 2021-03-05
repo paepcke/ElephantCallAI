b257bfc7f4a5702437cfa3423d9b79292f21d40b
jonathangomesselman <jgs8@stanford.edu>
2021-03-04 20:42:35 -0700

    Ready to test inference code

--- src/temp_spectrogramer.py
+++ src/temp_spectrogramer.py
@@ -36,6 +36,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
     data_dirs = args.data_dirs
     outputDir = args.outputDir
+    # Make sure this exists
+    if not os.path.exists(outputDir):
+        os.mkdir(outputDir)
+
     spectrogram_info = {'NFFT': args.NFFT,
                         'hop': args.hop,
                         'max_freq': args.max_freq,
