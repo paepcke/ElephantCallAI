81e319f8c1bc42612633bf0d94c6202ec7980361
jonathangomesselman <jgs8@stanford.edu>
2021-06-26 13:17:39 -0400

    Updated eval to include the new data and also work for marginal calls

--- src/eval.py
+++ src/eval.py
@@ -49,7 +49,7 @@ parser.add_argument('--call_preds_path', type=str, dest='call_predictions_path',
 # Defaults based on quatro
 parser.add_argument('--test_files', type=str, default='/home/data/elephants/processed_data/Test_nouab/Neg_Samples_x1/files.txt')
 
-parser.add_argument('--spect_path', type=str, default="/home/data/elephants/rawdata/Spectrograms/nouabale_general_test/", 
+parser.add_argument('--spect_path', type=str, default="/home/data/elephants/rawdata/Spectrograms/nouabale_test/", 
     help='Path to the processed spectrogram files')
 
 
@@ -842,12 +842,23 @@ def get_spectrogram_paths(test_files_path, spectrogram_path, exclude_marginals=F
         # Create the spectrogram path by concatenating
         # the test file with the path to the folder
         # containing the spectrogram files
+        '''
         paths['specs'].append(spectrogram_path + '/' + file + '_spec.npy')
         if not exclude_marginals:
             paths['labels'].append(spectrogram_path + '/' + file + '_label.npy')
         else:
             paths['labels'].append(spectrogram_path + '/' + file + '_marginal_label_mask.npy')
         paths['gts'].append(spectrogram_path + '/' + file + '_gt.txt')
+        '''
+
+        paths['specs'].append(spectrogram_path + '/' + file + '_000000_spectro.npy')
+        if  exclude_marginals:
+            paths['labels'].append(spectrogram_path + '/' + file + '_000000_marginal_label_mask.npy')
+        else:
+            paths['labels'].append(spectrogram_path + '/' + file + '_000000_label_mask.npy')
+
+        paths['gts'].append(spectrogram_path + '/' + file + '_gt.txt')  
+        
 
     return paths
     
